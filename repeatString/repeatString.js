cbc24a5856164b8706c1acc01a1a1f52a5281091
Patrick <patricklam500@gmail.com>
2021-05-09 09:28:16 -0700

    commit all

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringRepeat, numRepeat) {
+    let count = 0;
+    let answer = "";
 
+    if(numRepeat<0)
+        return "ERROR";
+    else {
+        while (count<numRepeat) {
+            answer += stringRepeat;
+            count++;
+        }
+    }
+    return answer;
 }
 
-module.exports = repeatString
+module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let inputStringLength = inputString.length;
+    let reString = new Array(inputStringLength);
+    let lastCount = inputStringLength - 1;
 
+    for (i=0; i<inputStringLength; i++){
+        
+        reString[i] = inputString[lastCount];
+        lastCount--;
+    }
+
+    return reString.join("");
 }
 
 module.exports = reverseString
