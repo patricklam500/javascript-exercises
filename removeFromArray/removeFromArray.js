0760cca17a8a347d7925f9782785a184dbbcf570
Patrick <patricklam500@gmail.com>
2021-05-09 09:38:35 -0700

    passed all tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function(inputArray, ...removeEles) {
+    let newArray = [];
+    let x = 0;
 
+    while (x <= removeEles.length) {
+        for (i=0; i<inputArray.length; i++) {
+            if(inputArray[i] === removeEles[x])
+                inputArray.splice(i,1);
+        }
+        x++;
+    }
+
+    return inputArray;
 }
 
 module.exports = removeFromArray
