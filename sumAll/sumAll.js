191de0dcd1fceba4393620173a354eb94cf2a925
Patrick <patricklam500@gmail.com>
2021-05-09 10:16:07 -0700

    passed all tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(beginNum, endNum) {
+    let result = 0;
 
+    //Error checking
+    if ((beginNum<0) || (endNum<0) || (isNaN(beginNum)) || (isNaN(endNum)) ||
+        (typeof(beginNum) == "string") || (typeof(endNum) == "string"))
+            return "ERROR";
+
+    //Sum all values between
+    if(beginNum > endNum) {
+        for(i=endNum; i<=beginNum; i++)
+            result += i;
+
+        console.log(result);
+        return result;
+    }
+
+    for(x=beginNum; x<=endNum; x++)
+        result += x;
+
+    console.log(result);
+    return result;
 }
 
 module.exports = sumAll
