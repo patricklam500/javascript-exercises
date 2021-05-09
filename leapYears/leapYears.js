fb5f94b93a3bcce235061ceff3f62c5895033b95
Patrick <patricklam500@gmail.com>
2021-05-09 10:35:07 -0700

    passed all tests

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(inYear) {
+    
+    //Error checking
+    if((inYear%100 == 0) && (inYear%400 != 0))
+        return false;
+    else if (((inYear%100 == 0) && (inYear%400 == 0)))
+        return true;
+    else if (inYear%4 == 0)
+        return true;
+    else
+        return false;
 
 }
 
 module.exports = leapYears
+
+//if divisible by 4, TRUE
+//if divisible by 100, FALSE done
+//if divisible by 100 AND 400, TRUE
